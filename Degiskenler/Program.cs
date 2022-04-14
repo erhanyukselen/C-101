63bc41a18f2cfbdfd5234c8a2e01ae5a69865ada
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-14 23:02:05 +0300

    Dönüşümler

---
+++ Degiskenler/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            //Değişken Dönüşümleri
+            string str20 = "20";
+            int int20 = 20;
+            string yeniDeger = str20 + int20.ToString();
+            Console.WriteLine(yeniDeger); //sonuç 2020
+
+            int int21=int20+Convert.ToInt32 (str20);
+            Console.WriteLine(int21); //sonuç 40
+
+            string datetime = DateTime.Now.ToString("dd.mm.yyyy");
+            Console.WriteLine(datetime);
+        }
+    }
+}

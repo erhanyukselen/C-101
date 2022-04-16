df20416ca654bf9afec0a1e32caa3e07def480b6
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-16 23:37:08 +0300

    Switch-Case

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak Ayındasınız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat Ayındasınız");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan Ayındasınız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart Ayındasınız");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış Mevsimindesiniz");
+                    break;
+
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar Mevsimindesiniz");
+                    break;
+
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimindesiniz");
+                    break;
+            }
+
+        }
+    }
+}

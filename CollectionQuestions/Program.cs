5d351d7805ee9fcfa43fb027b05b15f0e517e5fd
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-04-23 15:43:18 +0300

    Collection Questions

--- CollectionQuestions/Program.cs
+++ CollectionQuestions/Program.cs
@@ -85,3 +85,103 @@ Console.WriteLine($"Avg: {sumNotPrime / NotPrime.Count}");
 
 
 
+/* Question 2 
+ * 
+ */
+
+namespace CollectionsQuestion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen 20 adet sayının en küçük 3 değeri ve en büyük 3 değerini al
+            //her iki grubunda ortalamasını ver ortalama toplamlarını yazdır!
+            int[] numbers = new int[20];
+            int[] arrMax = new int[3];
+            int[] arrMin = new int[3];
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("Please Enter {0}. Number: ", i + 1);
+                int number = Convert.ToInt32(Console.ReadLine());
+                numbers[i] = number;
+            }
+
+            Array.Sort(numbers);
+            for (int i = 0; i < 3; i++)
+            {
+                arrMin[i] = numbers[i];
+            }
+            Console.WriteLine("Dizideki En küçük 3 Eleman");
+            foreach (var item in arrMin)
+            {
+                Console.WriteLine(item);
+            }
+            Array.Reverse(numbers);
+            for (int i = 0; i < 3; i++)
+            {
+                arrMax[i] = numbers[i];
+            }
+            double sumMax = 0;
+            foreach (var item in arrMax)
+            {
+                sumMax += item;
+            }
+            double sumMin = 0;
+            foreach (var item in arrMin)
+            {
+                sumMin += item;
+            }
+            double avgMax = sumMax / 3;
+            double avgMin = sumMin / 3;
+            Console.WriteLine("arrMax's Average : " + avgMax.ToString("F"));
+            Console.WriteLine("arrMin's Average : " + avgMin.ToString("F"));
+            Console.WriteLine("Sums of Averages : " + (avgMax + avgMin));
+
+        }
+    }
+}
+
+
+
+/* Question 3 */
+
+
+namespace CollectionsQuestions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden Girilen Cümle İçerisindeki Sesli harfleri al
+            //bir diziye aktar ve diziyi sıralı yazdır.
+            char[] vowels = { 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü' };
+            List<char> vowels1 = new();
+
+            Console.WriteLine("Please enter a sentence!");
+            string sentence = Console.ReadLine();
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (vowels.Contains(sentence[i]))
+                {
+                    vowels1.Add(sentence[i]);
+                }
+            }
+
+            vowels1.Sort();
+            vowels1.ToArray();
+
+            if (vowels1.Count == 0)
+                Console.WriteLine("There are no vowels in the entered sentence!");
+            else
+            {
+                Console.WriteLine("**** Vowels in sentence of entered ****");
+                foreach (var item in vowels1)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}

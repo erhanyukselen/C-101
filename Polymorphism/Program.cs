763f203faa5ae9901a349e2c573fdeed79687693
Erhan Ali Yükselen <102170410+erhanyukselen@users.noreply.github.com>
2022-05-15 21:46:46 +0300

    Polymorphism

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace Polymorphism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //              Canlılar
+        //                  |
+        //      Bitkiler            Hayvanlar
+        //  |             |       |            |
+        // Tohumlu  Tohumsuz   Surungenler  Kuslar+
+
+        TohumluBitkiler tohumluBitki = new TohumluBitkiler();
+        tohumluBitki.TohumlaCogalma();
+
+        Console.WriteLine("***************");
+
+        Kuslar marti = new Kuslar();
+        
+        marti.Ucmak();
+        }
+    }
+
+
+    
+}
